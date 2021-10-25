7219f9ef6857319937fe5f738eb9b375917cb501
NAVNARAY <NAVNARAY@NAVNARAY-LAP.in.oracle.com>
2021-10-25 14:24:02 +0530

    Uncommented line 83 to 85

--- src/test/java/com/oracle/acs/util/BrowserDriverUtil.java
+++ src/test/java/com/oracle/acs/util/BrowserDriverUtil.java
@@ -80,9 +80,9 @@ public class BrowserDriverUtil
     public static void init() {
         try {
 
-//            WebDriverManager.chromedriver().setup();
-//            WebDriver driver = new ChromeDriver();
-//            driver.get("http://www.total-qa.com");
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get("http://www.total-qa.com");
             System.out.println("Browser launched");
             try{
                 PropertyUtils.loadProperties(new FileInputStream("src/test/resources/propertyFiles/default/system-default.properties"),true);
